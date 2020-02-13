b2c5e8f6edf4177e008460f47c82913fd76ff40f
nishant570 <nishant.lohakare@gmail.com>
2020-02-14 02:26:37 +0530

    initial commit

---
+++ BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IEnumerable<Book> Book { get; set; }
+        public async Task OnGet()
+        {
+            Book = await _db.Book.ToListAsync();
+        }
+    }
+}
